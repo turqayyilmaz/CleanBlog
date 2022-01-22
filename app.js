5e2ae1c04def6b04ac469a2243e1dcb86656c3ab
turgay yılmaz <turqayyilmaz@gmail.com>
2022-01-22 16:28:18 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+
+  res.send(blog);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
